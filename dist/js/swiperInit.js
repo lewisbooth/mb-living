dc04bd1a40f3749ddf1529e79e6efb52100b47d6
Lewis Booth <lewis@LEWIS-RENDER.localdomain>
2019-11-12 16:21:31 +0000

    Upgraded to Gulp 4

---
+++ dist/js/swiperInit.js
@@ -0,0 +1,1 @@
+"use strict";function initSwiper(){new Swiper(".top-swiper .swiper-container",{speed:600,loop:!0,autoplay:{delay:4e3,disableOnInteraction:!1}}),new Swiper(".orchard-manor-swiper .swiper-container",{speed:600,loop:!0,autoplay:{delay:4e3,disableOnInteraction:!1}}),new Swiper(".corona-park-swiper .swiper-container",{speed:600,loop:!0,autoplay:{delay:4e3,disableOnInteraction:!1}})}
--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,73 @@
-var gulp = require("gulp");
-var pug = require("gulp-pug");
-var stylus = require("gulp-stylus");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var cssnano = require("cssnano");
-var babel = require("gulp-babel");
-var plumber = require("gulp-plumber");
-var minify = require("gulp-minify");
-var browserSync = require("browser-sync").create();
-
-gulp.task("build", ["stylus", "scripts", "pug-build"]);
-
-gulp.task("dev", ["stylus", "scripts", "pug"], function () {
-  browserSync.init({
-    server: {
-      baseDir: "./dist",
-      serveStaticOptions: {
-        extensions: ["html"]
-      }
-    }
-  });
-  gulp.watch("src/css/**/*.styl", ["stylus"]);
-  gulp.watch("src/js/**/*.js", ["scripts"]);
-  gulp.watch("src/pug/**/*.pug", ["pug"]);
-  gulp.watch("dist/**/*.html").on("change", browserSync.reload);
-  gulp.watch("dist/**/*.js", ["pug"]);
-});
+var gulp = require("gulp")
+var pug = require("gulp-pug")
+var stylus = require("gulp-stylus")
+var postcss = require("gulp-postcss")
+var autoprefixer = require("autoprefixer")
+var cssnano = require("cssnano")
+var babel = require("gulp-babel")
+var plumber = require("gulp-plumber")
+var minify = require("gulp-uglify")
+var browserSync = require("browser-sync").create()
 
 gulp.task("pug", function () {
   return gulp
     .src(["src/pug/**/*.pug", "!src/pug/_partials/**/*.pug"])
     .pipe(plumber())
     .pipe(pug())
-    .pipe(gulp.dest("dist"));
-});
+    .pipe(gulp.dest("dist"))
+})
 
 gulp.task("pug-build", function () {
   return gulp
     .src("src/pug/*.pug")
     .pipe(plumber())
     .pipe(pug())
-    .pipe(gulp.dest("dist"));
-});
+    .pipe(gulp.dest("dist"))
+})
 
 gulp.task("compile-scripts", function () {
   return gulp
     .src("src/js/**/*.js")
     .pipe(plumber())
-    .pipe(
-      babel({
-        presets: ["env"]
-      })
-    )
-    .pipe(
-      minify({
-        noSource: true,
-        ext: {
-          min: ".min.js"
-        }
-      })
-    )
-    .pipe(gulp.dest("dist/js"));
-});
+    .pipe(babel({ presets: ["env"] }))
+    .pipe(minify())
+    .pipe(gulp.dest("dist/js"))
+})
 
-gulp.task("scripts", ["compile-scripts"], function () {
-  browserSync.reload({ stream: true });
-});
+gulp.task("scripts", gulp.series("compile-scripts"), function () {
+  browserSync.reload({ stream: true })
+})
 
 gulp.task("stylus", function () {
   var plugins = [
     autoprefixer({ browsers: ["last 3 versions"] }),
     cssnano({ discardUnused: false })
-  ];
+  ]
   return gulp
     .src("src/css/style.styl")
     .pipe(plumber())
     .pipe(stylus())
     .pipe(postcss(plugins))
     .pipe(gulp.dest("dist/css"))
-    .pipe(browserSync.reload({ stream: true }));
-});
+    .pipe(browserSync.reload({ stream: true }))
+})
 
-gulp.task("default", ["dev"]);
+gulp.task("build", gulp.series("stylus", "scripts", "pug-build"))
+
+gulp.task("dev", gulp.series("stylus", "scripts", "pug"), function () {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+      middleware: function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        next()
+      }
+    }
+  })
+  gulp.watch("src/css/**/*.styl", gulp.series("stylus"))
+  gulp.watch("src/js/**/*.js", gulp.series("scripts", "pug"))
+  gulp.watch("src/pug/**/*.pug", gulp.series("pug"))
+  gulp.watch("dist/**/*.*").on("change", browserSync.reload)
+})
+
+gulp.task("default", gulp.series("dev"))
